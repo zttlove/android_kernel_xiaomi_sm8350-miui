74e29f574953e50d4130ad5b039a820b26242d0b
EndCredits <endcredits@crepuscular-aosp.icu>
2025-04-26 15:29:23 +0800

    build.sh: disable debugfs
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- build.sh
+++ build.sh
@@ -175,7 +175,7 @@ main(){
         generate_flashable
     elif [ $1 == "defconfig" ]
     then
-        DEFCONFIG_NAME="vendor/lahaina-qgki_defconfig vendor/xiaomi_QGKI.config vendor/${TARGET_DEVICE}_QGKI.config"
+        DEFCONFIG_NAME="vendor/lahaina-qgki_defconfig vendor/xiaomi_QGKI.config vendor/${TARGET_DEVICE}_QGKI.config vendor/debugfs.config"
         make_defconfig;
     else
         display_help
