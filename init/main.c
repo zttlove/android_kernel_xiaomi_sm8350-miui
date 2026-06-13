5cff9b13c1d5a43e25479dc201b35ab2c200df97
zttlove <115808960+zttlove@users.noreply.github.com>
2026-06-13 22:56:06 +0800

    Update main.c

--- init/main.c
+++ init/main.c
@@ -93,7 +93,8 @@
 #include <linux/cache.h>
 #include <linux/rodata_test.h>
 #include <linux/jump_label.h>
-#include <kernelsu/ksu.h>
+#include "../kernelsu/ksu.h"
+
 
 #include <asm/io.h>
 #include <asm/setup.h>
